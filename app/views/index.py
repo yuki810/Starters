84a8076782ff1aaa48d0f1455f523d898f006897
aya-ota-1101 <ota.a.ab@m.titech.ac.jp>
2023-09-23 17:25:31 +0900

    ota 09231700

--- app/views/index.py
+++ app/views/index.py
@@ -1,12 +1,12 @@
-from flask import Blueprint, render_template, request
+from flask import Blueprint, render_template, request, session, redirect, url_for
 # from app.code.code import checkMessage
 # from code.code import 
 import openai
-openai.api_key = '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'
+openai.api_key = ''
   
 
 def checkMessage(text):
-  openai.api_key = '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'
+  openai.api_key = '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'
   response = openai.ChatCompletion.create(
     model="gpt-4",
     messages=[
@@ -19,6 +19,22 @@ def checkMessage(text):
   a = response.choices[0].message['content'].strip()
   return a
 
+def alertMessage(text):
+  openai.api_key = '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'
+  response = openai.ChatCompletion.create(
+    model="gpt-4",
+    messages=[
+          {"role": "system", "content": "以下のコメントに対し,でちゅまちゅ口調で優しく諭して"},
+          {"role": "user", "content": "その時に返す言葉は30文字以内にしてほしい"},
+          {"role": "user", "content": text},
+      ],
+    temperature=0.1
+  )
+
+  a = response.choices[0].message['content'].strip()
+  return a
+
+
 view = Blueprint('index', __name__, url_prefix='/')
 @view.route('/', methods=['GET'])
 def show():
@@ -26,8 +42,15 @@ def show():
 
 @view.route('/check', methods=['POST'])
 def checktweet():
-    print("a")
-    # data = request.form.get('key')
-    # isOK = checkMessage(data)
-    isOK = "a"
-    return isOK
+    text = request.form['text_area']
+    isOK = checkMessage(text)
+
+    print(text)
+    print(isOK)
+
+    if isOK == "TRUE":
+       a = alertMessage(text)
+       print(a)
+       return render_template('alert.html', alert = a)
+    else:
+       return redirect(url_for('/')) 
