36ccc1b7c7c8d8d842b25e0cd3824c0e372482a5
aya-ota-1101 <ota.a.ab@m.titech.ac.jp>
2023-09-23 13:16:58 +0900

    app engine

--- app/code/code.py
+++ app/code/code.py
@@ -1,2 +1,11 @@
-def checkTweet(username):
-    print(username)
+def checkMessage(text):
+  response = openai.ChatCompletion.create(
+    model="gpt-4",
+    messages=[
+          {"role": "system", "content": "以下のコメントに対し不適切であるかどうか、TRUE or FALSEで答えて"},
+          {"role": "user", "content": text},
+      ],
+    temperature=0.1
+  )
+
+  print(response.choices[0].message['content'].strip())
